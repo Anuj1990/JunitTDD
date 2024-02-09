4e2bdbc0e6bba6085ded51a3eb654905d220f2ea
parthagoku <50693658+parthagoku@users.noreply.github.com>
2024-02-09 10:38:50 +0530

    Update MathsTest.java
    just testing

--- TestSrc/com/tddJunit/java/MathsTest.java
+++ TestSrc/com/tddJunit/java/MathsTest.java
@@ -6,6 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class MathsTest {
+	private String lauda;
 
 	private Maths classundertest;	
 
@@ -32,5 +33,7 @@ public class MathsTest {
 	assertEquals(result, classundertest.subtract(10,2));
 
 	}
+
+	hello blah blah blahhh
 
 }
